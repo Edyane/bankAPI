4b093c19da8d08713b5655493801a255383a942e
Edyane <edyanealves@gmail.com>
2023-09-07 09:42:02 -0300

    feat: account registration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+app.use(express.json());
+
+const costumers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid (identificador único universal)
+ * statement - array (extrato = todos os créditos e débitos)
+ */
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+    const id = uuidv4();
+    costumers.push({
+        cpf, 
+        name,
+        id,
+        statement: []
+    });
+
+    return response.status(201).send();
+});
 
 app.listen(3333);
