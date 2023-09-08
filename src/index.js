b934dab0629733b02bfd2c7e78649a484c00d3e6
Edyane <edyanealves@gmail.com>
2023-09-08 10:42:36 -0300

    feat: listing statement

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ const customers = [];
  */
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
-
     const customerAlreadyExists = customers.some(
         (customer) => customer.cpf === cpf
     );
@@ -33,5 +32,12 @@ app.post("/account", (request, response) => {
 
     return response.status(201).send();
 });
+
+app.get("/statement/:cpf", (request, response) => {
+    const { cpf } = request.params;
+    const customer = customers.find(customer => customer.cpf === cpf);
+    
+    return response.json(customer.statement);
+})
 
 app.listen(3333);
